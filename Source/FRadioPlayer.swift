ae6d5844e8461e289abc417a917514e35492d1c0
Celso Barriga <cbarriga@gmail.com>
2019-07-17 19:29:03 -0400

    Revert "1) Set the player status early on before sending the signal that the metadata has changed. This will allow for a case when some logic is necessary on what to do when a metadata has changed based on whether the player is playing or stopped. 2) On centova streams and I believe also for shoutcast, some trailing characters are inserted for tracking purposes. This makes the searching for artworks based on title and artist fail because of those extra characters, so we want to remove them first before sending them out."
    This reverts commit 113979dd48e40011a28273d4520b677d1e26c491.

--- Source/FRadioPlayer.swift
+++ Source/FRadioPlayer.swift
@@ -283,9 +283,9 @@ open class FRadioPlayer: NSObject {
      */
     open func stop() {
         guard let player = player else { return }
-        playbackState = .stopped
         player.replaceCurrentItem(with: nil)
         timedMetadataDidChange(rawValue: nil)
+        playbackState = .stopped
     }
     
     /**
@@ -399,14 +399,7 @@ open class FRadioPlayer: NSObject {
             return
         }
         
-        // Strip off trailing '[???]' characters left there by ShoutCast and Centova Streams
-        // It will leave the string alone if the pattern is not there
-        let rawCleaned = NSMutableString(string: rawValue)
-        let pattern = "(\\[.*?\\]\\w*$)"
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
-        regex.replaceMatches(in: rawCleaned , options: .reportProgress, range: NSRange(location: 0, length: rawCleaned.length), withTemplate: "")
-        
-        FRadioAPI.getArtwork(for: rawCleaned as String, size: artworkSize, completionHandler: { [unowned self] artworlURL in
+        FRadioAPI.getArtwork(for: rawValue, size: artworkSize, completionHandler: { [unowned self] artworlURL in
             DispatchQueue.main.async {
                 self.delegate?.radioPlayer?(self, artworkDidChange: artworlURL)
             }
